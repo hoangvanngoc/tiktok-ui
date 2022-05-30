718eea10bb37efcb563beda77166ae895c480592
hoangvanngoc <hoangngocbkhn2311@gmail.com>
2022-05-31 00:14:05 +0700

    buil layout and router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route }  from 'react-router-dom'
-import { publicRoutes } from '~/routes/index'
+import { publicRoutes } from '~/routes'
+import { DefaultLayout } from '~/components/Layout'
 
 function App() {
   return (
@@ -8,10 +10,20 @@ function App() {
           <Routes>
             { publicRoutes.map((route, index) => {
                const Page = route.component
-               return <Route key={index} path={route.path} element={Page}/>
+               let Layout = DefaultLayout
+
+               if (route.layout) {
+                 Layout = route.layout
+               } else if (route.layout === null) {
+                 Layout = Fragment
+               }
+               return <Route key={index} path={route.path} element=
+               {
+                <Layout>
+                  <Page/>
+                </Layout>
+                }/>
             })}
-            {/* <Route path='/' element={Home}/>
-            <Route path='/following' element={Following}/> */}
           </Routes>
       </div>
     </Router>
